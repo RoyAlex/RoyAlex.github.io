eec35e03020a3278d06955bb90087f2eccb1a71a
RoyAlex <alexandre.roy90@gmail.com>
2015-02-15 16:22:21 -0500

    Ajout de google map api et transfere du javascript dans un fichier

---
+++ js/myJs.js
@@ -0,0 +1,77 @@
+(function() {
+	
+
+  $(document).ready(function() {   
+    section = $('.section');
+    mywindow = $(window);
+    var mainbottom = $('#header-menu').offset().top + $('#header-menu').height(); 
+    //Set up for waypoints navigation
+    section.waypoint( function (direction) {
+      element = $(this).attr("element")
+      //cache the variable of the data-section attribute associated with each section
+      nosection = $(element).attr('nosection');
+      if (direction === 'down') {
+          $('.navigation li[nosection="' + nosection + '"]').addClass('active').prev().removeClass('active');
+      }
+
+    }, {  offset: "25%"});
+
+    section.waypoint( function (direction) {
+      element = $(this).attr("element")
+      //cache the variable of the data-section attribute associated with each section
+      nosection = $(element).attr('nosection');
+      if (direction === 'up') {
+          $('.navigation li[nosection="' + nosection + '"]').addClass('active').next().removeClass('active');
+      }
+
+    }, {  offset: -50});
+
+    mywindow.scroll(function () {
+      if (mywindow.scrollTop() == 0) {
+        $('.navigation li[nosection="1"]').addClass('active');
+        $('.navigation li[nosection="2"]').removeClass('active');
+      }
+      stop = Math.round($(window).scrollTop());
+      var $this = $('.navbar');
+      if(stop > mainbottom){
+        if ($this.hasClass('navbar-default')) {
+          $this.removeClass('navbar-default');
+          $('.navbar').addClass('navbar-inverse');
+        }
+      }
+      else 
+      {
+       if ($this.hasClass('navbar-inverse')) {
+          $this.removeClass('navbar-inverse');
+          $('.navbar').addClass('navbar-default');
+        }
+      } 
+    });
+
+
+    $('.navbar li').click(function(e) {
+        $('.navbar li.active').removeClass('active');
+        var $this = $(this);
+        if (!$this.hasClass('active')) {
+            $this.addClass('active');
+        }
+        e.preventDefault();
+        $.scrollTo(
+          $('.navbar li.active a').attr("href"),
+          {
+            duration: 1000,
+          }
+        );
+    });
+   /* function initialize() {
+      var mapOptions = {
+        center: { lat: -34.397, lng: 150.644},
+        zoom: 8
+      };
+      var map = new google.maps.Map(document.getElementById('map-canvas'),
+          mapOptions);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);*/
+
+  });
+}());
